2f5286414f270e0127bb4a91457bed22420690d6
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-05-26 16:10:19 -0400

    cereated login and sign up modal

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,15 @@ import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import Project from "./pages/Project";
 import Footer from "./components/Footer";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp"
 
 function App() {
   return (
     <div className="App">
       <main>
+        <Login />
+        <SignUp />
         <Home />
         <Dashboard />
         <Project />
@@ -20,3 +24,9 @@ function App() {
 }
 
 export default App;
+
+{/* <div className="w-screen py-10 flex items-center justify-center">
+<button className="py-2 px-5 rounded  bg-blue-500 text-white">
+  Open Modal
+</button>
+</div> */}
