53bda7037de08ce58dcb9ed3501df6de9362b813
Mick L <90425599+Micklitodev@users.noreply.github.com>
2023-05-26 22:37:54 -0400

    0.2.1
    Updated /config/connection.js to have console.log that notifies the db was sucesfully connected, updated /models/user.js to have virtuals and to utilize the bcrypt package so that sensitive information would be hashed prior to creation of an instance of the user. Updated /model/project.js to have a projectId field, title, and pass in the tasks schema to projects tasks key inside of projects, Created a /models/tasks.js that will refrence the projectId, have a task sate, taskbody, difficulty and task owner. Uncommented db connection related code inside of server.js.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,11 @@
 //connection instance live here 
 const mongoose = require('mongoose');
 
+mongoose.set('strictQuery', false)
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/project3DB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+}).then(() => console.log('connectedtomongodb'))
 
 module.exports = mongoose.connection;
