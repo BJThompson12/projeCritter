035ff3507bdc788acceb7c301dd37d751662fe56
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-05-31 20:08:10 -0400

    0.8.3

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # Project 3 
+
+   <div className='relative flex flex-col overflow-hidden'>
+          <div className='w-full p-6 m-auto bg-white rounded-md lg:max-w-xl mt-0'>
+            <h1 className='text-3xl font-semibold text-center text-indigo-500'>
+              Log in
+            </h1>
+            <form className='mt-6'>
+              <div className='mb-2'>
+                <label
+                  for='email'
+                  className='block text-sm font-semibold text-indigo-500 text-center'
+                >
+                  Email
+                </label>
+                <input
+                  type='email'
+                  className='block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40'
+                />
+              </div>
+              <div className='mb-2'>
+                <label
+                  for='password'
+                  className='block text-sm font-semibold text-indigo-500 text-center'
+                >
+                  Password
+                </label>
+                <input
+                  type='password'
+                  className='block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40'
+                />
+              </div>
+              <a href='#' className='text-xs text-indigo-500 hover:underline'>
+                Forget Password?
+              </a>
+              <div className='mt-6 flex flex-col'>
+                <button className='w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-indigo-500 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600 sm:mt-12'>
+                  Login
+                </button>
+              </div>
+            </form>
+
+            <p className='mt-8 text-xs font-light text-center text-gray-700'>
+              {' '}
+              Don't have an account?{' '}
+              <a
+                href=''
+                className='font-medium text-indigo-500 hover:underline'
+              >
+                Sign up Here!
+              </a>
+            </p>
+          </div>
+        </div>
+    </>
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,58 +1,107 @@
+import { useMutation } from '@apollo/client';
+import { LOG_IN } from '../utils/mutation';
+import Auth from '../utils/auth';
+
+import { useState } from 'react';
+
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [showAlert, setShowAlert] = useState(false);
+
+  const [login] = useMutation(LOG_IN);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await login({
+        variables: { ...formData },
+      });
+
+      console.log(data);
+
+      if (!data) {
+        throw new Error('something went wrong!');
+      }
+
+      Auth.login(data.login.token);
+    } catch (err) {
+      console.error(err);
+      setShowAlert(true);
+    } finally {
+      setFormData({
+        email: '',
+        password: '',
+      });
+    }
+  };
+
   return (
     <>
-    <div className='relative flex flex-col overflow-hidden'>
+         <div className='relative flex flex-col overflow-hidden'>
           <div className='w-full p-6 m-auto bg-white rounded-md lg:max-w-xl mt-0'>
             <h1 className='text-3xl font-semibold text-center text-indigo-500'>
               Log in
             </h1>
-            <form className='mt-6'>
-              <div className='mb-2'>
-                <label
-                  for='email'
-                  className='block text-sm font-semibold text-indigo-500 text-center'
-                >
-                  Email
-                </label>
-                <input
-                  type='email'
-                  className='block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40'
-                />
-              </div>
-              <div className='mb-2'>
-                <label
-                  for='password'
-                  className='block text-sm font-semibold text-indigo-500 text-center'
-                >
-                  Password
-                </label>
-                <input
-                  type='password'
-                  className='block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40'
-                />
-              </div>
-              <a href='#' className='text-xs text-indigo-500 hover:underline'>
-                Forget Password?
-              </a>
-              <div className='mt-6 flex flex-col'>
-                <button className='w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-indigo-500 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600 sm:mt-12'>
-                  Login
-                </button>
-              </div>
-            </form>
-
-            <p className='mt-8 text-xs font-light text-center text-gray-700'>
-              {' '}
-              Don't have an account?{' '}
-              <a
-                href=''
-                className='font-medium text-indigo-500 hover:underline'
+          <form onSubmit={handleSubmit} className='mt-6'>
+            <div className='mb-2'>
+              <label
+                for='email'
+                className='block text-sm font-semibold text-indigo-500 text-center'
               >
-                Sign up Here!
-              </a>
-            </p>
-          </div>
+                Email
+              </label>
+              <input
+                type='email'
+                placeholder='Your email address'
+                name='email'
+                onChange={handleInputChange}
+                value={formData.email}
+                required
+                className='block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40'
+              />
+            </div>
+            <div className='mb-2'>
+              <label
+                for='password'
+                className='block text-sm font-semibold text-indigo-500 text-center'
+              >
+                Password
+              </label>
+              <input
+                placeholder='Your password'
+                name='password'
+                onChange={handleInputChange}
+                value={formData.password}
+                required
+                type='password'
+                className='block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40'
+              />
+            </div>
+            <div className='mt-6'>
+              <button
+                className='w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-indigo-500 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600'
+                disabled={
+                  !(formData.email && formData.password)
+                }
+                type='submit'
+                variant='success'
+              >
+                Login
+              </button>
+            </div>
+          </form>
         </div>
+      </div>
     </>
   );
 };
