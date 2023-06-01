f3afa609fbe86f81ea7b9136210793e649c19813
C K Boyt <ckboyt@gmail.com>
2023-05-31 21:35:11 -0400

    0.8.1
    Revise navbar styling.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
-import Auth from '../utils/auth';
+import React, { useState, useEffect } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import Auth from "../utils/auth";
 
-import Login from './Login';
-import SignUp from './SignUp';
-import { logMissingFieldErrors } from '@apollo/client/core/ObservableQuery';
+import Login from "./Login";
+import SignUp from "./SignUp";
+import { logMissingFieldErrors } from "@apollo/client/core/ObservableQuery";
 
-const loggedIn = Auth.getToken()
-const authState = {state: loggedIn}
-console.log(authState.state)
-
+const loggedIn = Auth.getToken();
+const authState = { state: loggedIn };
+console.log(authState.state);
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -17,102 +16,123 @@ const Navbar = () => {
 
   const handleClick = () => setNav((prevNav) => !prevNav);
 
+  // styles
+  const navLink = "text-xl text-white hover:underline";
+  const navButton =
+    "px-4 py-2 text-lg font-medium text-indigo-500 rounded-md bg-white hover:bg-indigo-200";
+
   return (
-    <nav>
-      <div className='w-screen h-[100px] z-10 bg-indigo-500 sticky top-0 drop-shadow-md'>
-        <div className='px-2 flex justify-between items-center w-full h-full'>
-          <div
-            className='text-slate-100 px-4'
+    <div className="sticky top-0 z-10 w-full ">
+      <header className="w-full h-20 bg-indigo-500 drop-shadow-md">
+        <div className="flex items-center justify-between w-full h-full px-2">
+          {/* site name container */}
+          <a
+            href="/"
+            className="px-4 text-slate-100 hover:text-indigo-200"
             style={{
-              fontFamily: 'Sniglet, cursive',
+              fontFamily: "Sniglet, cursive",
               fontWeight: 800,
-              fontSize: '40px',
+              fontSize: "40px",
             }}
           >
-            projectCritter
-          </div>
-          <ul className='hidden md:flex space-x-4 text-slate-100'>
-            <li>About</li>
-            <div>
-              {Auth.loggedIn ? (
+            projeCritter
+          </a>
+
+          {/* desktop navigation */}
+          <nav>
+            <ul className="hidden space-x-4 md:flex md:items-center">
+              <li>
+                <a href="/about" className={navLink}>
+                  About
+                </a>
+              </li>
+              {authState.state ? (
                 <>
-                  <li>Dashboard</li>
                   <li>
-                    <button className='px-4 py-2 bg-white text-indigo-500 '>
-                      Sign Out
-                    </button>
+                    <a href="/dashboard" className={navLink}>
+                      Dashboard
+                    </a>
+                  </li>
+                  <li>
+                    <button className={navButton}>Log Out</button>
                   </li>
                 </>
               ) : (
-                <>
+                <li>
                   <button
-                    className='bg-white text-indigo-500 rounded-md px-8 py-2 text-base font-medium hover:bg-blue-100
-    focus:outline-none focus:ring-2 focus:ring-blue-500'
+                    className={navButton}
                     onClick={() => setDisplayModal(displayModal ? false : true)}
                   >
-                    Sign up/Log in
+                    Sign Up / Log In
                   </button>
-                </>
+                </li>
               )}
-            </div>
-          </ul>
-          <div className='md:hidden' onClick={handleClick}>
+            </ul>
+          </nav>
+
+          {/* mobile menu button */}
+          <div className="md:hidden" onClick={handleClick}>
             {!nav ? (
-              <Bars3Icon className='w-6' />
+              <Bars3Icon className="w-8 text-white" />
             ) : (
-              <XMarkIcon className='w-6' />
+              <XMarkIcon className="w-8 text-white" />
             )}
           </div>
         </div>
+
+        {/* mobile navigation */}
         <ul
           className={
             !nav
-              ? 'hidden'
-              : 'absolute bg-indigo-500  text-slate-100 w-full px-8 md:hidden'
+              ? "hidden"
+              : "absolute bg-indigo-500 text-slate-100 w-full px-8 pb-4 md:hidden space-y-2 text-right"
           }
         >
-          <li>About</li>
+          <li>
+            <a href="/about" className={navLink}>
+              About
+            </a>
+          </li>
           {authState.state ? (
             <>
-              <li>Dashboard</li>
               <li>
-                <button>Sign Out</button>
+                <a href="/dashboard" className={navLink}>
+                  Dashboard
+                </a>
+              </li>
+              <li>
+                <button className={navButton}>Log Out</button>
               </li>
             </>
           ) : (
-            <>
-              <div className='py-3'>
-                <button className='px-4 py-2 bg-white text-indigo-500 '>
-                  Sign up/Log in
-                </button>
-              </div>
-            </>
+            <button className={navButton}>Sign Up / Log In</button>
           )}
         </ul>
-      </div>
+      </header>
 
+      {/* login modal */}
       {displayModal ? (
         <modal>
           <div>
-            <div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none'>
-              <div className='relative w-auto my-6 mx-auto max-w-3xl'>
+            <div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none">
+              <div className="relative w-auto max-w-3xl mx-auto my-6">
                 {/*content*/}
-                <div className='border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
+                <div className="relative flex flex-col w-full bg-white border-0 rounded-lg shadow-lg outline-none focus:outline-none">
                   {/*body*/}
 
-                  <div className=' p-10'>
+                  <div className="p-10 ">
                     <div>
-                      <div className='display flex flex-wrap gap-1 justify-center px-2'>
-                        {' '}
-                        <SignUp /> <Login />{' '}
+                      <div className="flex flex-wrap justify-center gap-1 px-2 display">
+                        {" "}
+                        <SignUp /> <Login />{" "}
                       </div>
                     </div>
                   </div>
                   {/*footer*/}
-                  <div className='flex items-center justify-center p-2 border-t border-solid border-slate-200 rounded-b'>
+                  <div className="flex items-center justify-center p-2 border-t border-solid rounded-b border-slate-200">
                     <button
-                      className='text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
-                      type='button'
+                      className="px-6 py-2 mb-1 mr-1 text-sm font-bold text-red-500 uppercase transition-all duration-150 ease-linear outline-none background-transparent focus:outline-none"
+                      type="button"
                       onClick={() => setDisplayModal(false)}
                     >
                       Close
@@ -121,11 +141,11 @@ const Navbar = () => {
                 </div>
               </div>
             </div>
-            <div className='opacity-25 fixed inset-0 z-40 bg-black'></div>
+            <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
           </div>
         </modal>
       ) : null}
-    </nav>
+    </div>
   );
 };
 
