0ce42f5cbf452d4abaf71f8570eabd398f084a08
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-06-05 19:07:32 -0400

    made needed changes to faq page icons

--- client/src/pages/Faq.jsx
+++ client/src/pages/Faq.jsx
@@ -1,3 +1,5 @@
+import { RiQuestionLine} from "react-icons/ri";
+
 const Faq = () => {
   return (
     <section className='bg-white '>
@@ -9,20 +11,9 @@ const Faq = () => {
         <div className='grid grid-cols-1 gap-8 mt-8 lg:mt-16 md:grid-cols-2 xl:grid-cols-3'>
           <div>
             <div className='inline-block p-3 text-white bg-indigo-500 rounded-lg'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
+              <RiQuestionLine
                 className='w-6 h-6'
-                fill='none'
-                viewBox='0 0 24 24'
-                stroke='currentColor'
-              >
-                <path
-                  stroke-linecap='round'
-                  stroke-linejoin='round'
-                  stroke-width='2'
-                  d='M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
-                />
-              </svg>
+              />
             </div>
 
             <div>
@@ -42,20 +33,9 @@ const Faq = () => {
 
           <div>
             <div className='inline-block p-3 text-white bg-indigo-500 rounded-lg'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
+            <RiQuestionLine
                 className='w-6 h-6'
-                fill='none'
-                viewBox='0 0 24 24'
-                stroke='currentColor'
-              >
-                <path
-                  stroke-linecap='round'
-                  stroke-linejoin='round'
-                  stroke-width='2'
-                  d='M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
-                />
-              </svg>
+              />
             </div>
 
             <div>
@@ -75,20 +55,9 @@ const Faq = () => {
 
           <div>
             <div className='inline-block p-3 text-white bg-indigo-500 rounded-lg'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
+            <RiQuestionLine
                 className='w-6 h-6'
-                fill='none'
-                viewBox='0 0 24 24'
-                stroke='currentColor'
-              >
-                <path
-                  stroke-linecap='round'
-                  stroke-linejoin='round'
-                  stroke-width='2'
-                  d='M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
-                />
-              </svg>
+              />
             </div>
 
             <div>
@@ -108,20 +77,9 @@ const Faq = () => {
 
           <div>
             <div className='inline-block p-3 text-white bg-indigo-500 rounded-lg'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
+            <RiQuestionLine
                 className='w-6 h-6'
-                fill='none'
-                viewBox='0 0 24 24'
-                stroke='currentColor'
-              >
-                <path
-                  stroke-linecap='round'
-                  stroke-linejoin='round'
-                  stroke-width='2'
-                  d='M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
-                />
-              </svg>
+              />
             </div>
 
             <div>
@@ -141,20 +99,9 @@ const Faq = () => {
 
           <div>
             <div className='inline-block p-3 text-white bg-indigo-500 rounded-lg'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
+            <RiQuestionLine
                 className='w-6 h-6'
-                fill='none'
-                viewBox='0 0 24 24'
-                stroke='currentColor'
-              >
-                <path
-                  stroke-linecap='round'
-                  stroke-linejoin='round'
-                  stroke-width='2'
-                  d='M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
-                />
-              </svg>
+              />
             </div>
 
             <div>
@@ -174,20 +121,9 @@ const Faq = () => {
 
           <div>
             <div className='inline-block p-3 text-white bg-indigo-500 rounded-lg'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
+            <RiQuestionLine
                 className='w-6 h-6'
-                fill='none'
-                viewBox='0 0 24 24'
-                stroke='currentColor'
-              >
-                <path
-                  stroke-linecap='round'
-                  stroke-linejoin='round'
-                  stroke-width='2'
-                  d='M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
-                />
-              </svg>
+              />
             </div>
 
             <div>
