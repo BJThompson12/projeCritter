1a0c22dc5cbc4735121c1d978314a18ca72b3766
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-06-08 19:09:03 -0400

    1.0.6

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,10 +7,10 @@ const Home = () => {
           src="https://images.unsplash.com/photo-1555212697-194d092e3b8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
         />
         <div className="w-full mt-8 text-left md:w-6/12 md:pl-10">
-          <h3 className="text-3xl font-semibold text-indigo-500">
+        <h3 className="text-3xl font-semibold bg-indigo-300 inline-block p-2 rounded-lg text-white border-2 border-r-6 border-b-6  border-black text-gray-900">
             projeCritter
           </h3>
-          <p className="mt-4 text-xl leading-relaxed text-blueGray-500">
+          <p className="mt-4 text-lg leading-relaxed bg-indigo-300 inline-block p-2 rounded-lg border-2  border-r-4 border-b-4  border-black text-gray-900">
           Tired of feeling overwhelmed with your tasks? Let our project task tracker tool be your personal assistant and help you conquer your to-do list!
           </p>
         </div>
